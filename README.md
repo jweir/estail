315047d63de085b19e233045020f25253e7280eb
J. Weir <john@smokinggun.com>
2013-01-23 11:35:44 -0500

    damn it is cold today

---
+++ README.md
@@ -0,0 +1,33 @@
+Tail (or pipe) anything to a web browser or web browsers.
+
+## ESTail (Event Source Tail)
+
+I want to watch some log files on remote browser, this makes it easy.
+
+Useage
+
+    tail -f somefile.log | ./estail
+
+Open the browser to localhost:8080
+
+Need to clear the buffer? Refresh the browser.
+
+### Get and Build
+
+Requires [Go](http://golang.org/doc/install) to be installed
+
+    git clone git@github.com:jwier/estail.git
+    cd estail
+    go get && go build
+
+You should now have a binary file `estail`
+
+### License 
+
+Copyright (c) 2013 John Weir
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ estail.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+  "log"
+	"bufio"
+	"fmt"
+	eventsource "github.com/antage/eventsource/http"
+	"net/http"
+	"os"
+)
+
+var Stdin *bufio.Reader
+
+func main() {
+	es := eventsource.New(nil)
+	defer es.Close()
+	http.Handle("/log/", es)
+	http.HandleFunc("/", index)
+
+	go func() {
+		for {
+			line, _, err := Stdin.ReadLine()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			es.SendMessage(string(line), "", "")
+		}
+	}()
+
+  log.Println("ESTail starting at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func init() {
+	Stdin = bufio.NewReader(os.Stdin)
+}
+
+func index(w http.ResponseWriter, r *http.Request) { 
+    fmt.Fprintf(w, template())
+}
+
+func template() string {
+  return `
+  <!DOCTYPE html>
+
+  <html>
+  <head>
+  <title>ESTail</title>
+  <link href='http://fonts.googleapis.com/css?family=Droid+Sans+Mono' rel='stylesheet' type='text/css'>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  <style type="text/css">
+  body {
+    margin: 20px;
+    font: 12px/18px 'Droid Sans Mono', san-serif;
+    color: #CCCCAA;
+    background: #222;
+  }
+
+  hr {
+    border:0;
+    height: 1px;
+    line-height: 1px;
+    border-top: 1px #590 solid;
+    margin: 9px 0 10px;
+  }
+
+  </style>
+  </head>
+
+  <body>
+  <h4>ESTail</h4>
+  <div id="log"></div>
+
+  <script>
+  (function(){
+
+    // clean up some shell color codes
+    function parse(s){
+      return s.replace(/\[\d{1,}m/img,"")
+    }
+
+    function listen(channel, target){
+      var source = new EventSource('/log/');
+      var addMark = false;
+
+      setInterval(function(){
+        console.log("hi", addMark)
+        if(addMark){ target.append($("<hr/>")); addMark = false}
+      },3000)
+
+      source.onmessage = function(e){
+        addMark = true;
+        target.append($("<div>"+parse(e.data)+"</div>"))
+        $(document).scrollTop($(document).height())
+      }
+    }
+
+    listen("log", $("#log"))
+  })()
+  </script>
+  </body>
+
+  </html>
+  `
+}
